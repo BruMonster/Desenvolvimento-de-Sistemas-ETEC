a4e2466ff1c39e3165692720aa80fa2e7c80f537
Bruno Gabriel <52282554+BruMonster@users.noreply.github.com>
2019-07-31 20:41:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Escreva sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("\nMeu Nome é " + nome + " e tenho " + idade + " anos");
+            Console.ReadKey();
+        }
+    }
+}
